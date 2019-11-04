f1bde1a08a9687bcaf40d3dcbba4896c5790d23a
ThomasAbreuDias <abreudias@gmail.com>
2019-11-04 12:06:33 +0100

    Upgrade

--- rpn.c
+++ rpn.c
@@ -1,5 +1,5 @@
 /**-Auteur : Thomas ABREU DIAS aka BOYA-**/
-/*-Dépot GIT :
+/*-Dépot GIT : https://github.com/ThomasAbreuDias/RPNhttps://github.com/ThomasAbreuDias/RPN
 */
 #include <stdlib.h>
 #include <stdio.h>
@@ -27,32 +27,15 @@ char* is_instruction(char* instruction){
 
 
 //Crée la liste
-_link* make_link(char* ope, int val, _link* next){
-
-  if(val  != 0 && ope != NULL )
-  {
-    fprintf(stderr, "Only one of the attribut need to don't be NULL\n");
-    exit(2);
-  }
-
+_link* make_link(int val, _link* next){
   _link* link = (_link*)malloc(sizeof(_link));
+
   if(link == NULL)
   {
     fprintf(stderr, "Out of memory\n");
     exit(1);
   }
 
-  if(ope != NULL){
-    link->operation = (char*) malloc(sizeof(char)*4);
-    if(link->operation == NULL)
-    {
-      fprintf(stderr, "Out of memory\n");
-      exit(1);
-    }
-    strcpy(link->operation, ope);
-  }
-
-  link->operation = NULL;
   link->value = val;
   link->next = next;
 
@@ -60,10 +43,7 @@ _link* make_link(char* ope, int val, _link* next){
 }
 //Affiche la liste
 void display_link(_link* list){
-  if (list->operation == NULL)
     printf("%d\n", list->value);
-  else
-    printf("%s\n", list->operation );
   if (list->next != NULL)
    display_link(list->next);
 }
@@ -72,42 +52,36 @@ void free_link(_link* list)
 {
   if (list != NULL)
   {
-    if (list->operation !=NULL)
-    {
-
-      free(list->operation); // pb de free un char non free !!??
-    }
-
-
     free_link(list->next);
     free(list);
   }
 }
 //Retire le premier element de la liste
 _link* pop_link(_link* list){
- if (list == NULL)
- {
-  fprintf(stderr, "Empty list\n");
-  exit(3);
-}
-_link* tmp = list->next;
-free(list);
-return tmp;
+   if (list == NULL)
+   {
+    fprintf(stderr, "Empty list\n");
+    exit(3);
+  }
+  _link* tmp = list->next;
+  free(list);
+  return tmp;
 }
 
 //Convertie les valeur en int
-_link* init_link(_link* list, char* instruction){
-  if (is_instruction(instruction) != NULL)
+_link* init_link(_link* list, char* value){
+  if (isdigit(value[0]))
   {
-   return make_link(instruction, 0, list);
- }else
- {
-  int n;
-  n = (int) strtol(instruction, NULL, 10);
+    int n;
+    n = (int) strtol(value, NULL, 10);
 
-  return make_link(NULL, n, list);
+    return make_link(n, list);
+  }else{
+    fprintf(stderr, "Value is not allowed\n");
+    exit(3);
+  }
 }
-}
+
 //Recupère la n valeur
 int get_link_n_last_value(int n, _link* list){
   if (list == NULL)
@@ -115,90 +89,64 @@ int get_link_n_last_value(int n, _link* list){
     fprintf(stderr, "Empty list\n");
     exit(3);
   }
-  if(n <= 0){
-    if (list->operation == NULL)
-    {
-      return list->value;
-    }
-  }
+  if(n <= 0)
+    return list->value;
   if (list->next != NULL)
     return get_link_n_last_value(n-1, list->next);
   return n;
 }
 
 //Execute les instructions
-_link* do_instruction(_link* list){
+_link* do_instruction(_link* list, char* instruction){
   char op[9][4] = {"ADD", "SUB", "MUL", "DIV", "MOD", "POP", "DUP", "SWP", "ROL"};
-  int result;
-  _link* newlink;
-  if (strcmp(list->operation, op[0]) == 0)
-  {
-    list = pop_link(list);
-    result = get_link_n_last_value(0, list);
-    list = pop_link(list);
-    result += get_link_n_last_value(0, list);
-    list = pop_link(list);
-    newlink = make_link(NULL, result, list);
+  int result = 0;
+  _link* newlink = NULL;
 
-  }else if (strcmp(list->operation, op[1]) == 0)
+  if (strcmp(instruction, op[0]) == 0)
   {
-    list = pop_link(list);
-    result = get_link_n_last_value(0, list);
-    list = pop_link(list);
-    result -= get_link_n_last_value(0, list);
-    list = pop_link(list);
-    newlink = make_link(NULL, result, list);
-  }else if (strcmp(list->operation, op[2]) == 0)
-  {
-    int tmp;
-    list = pop_link(list);
-    result = get_link_n_last_value(0, list);
-    list = pop_link(list);
-    tmp = get_link_n_last_value(0, list);
-    if (tmp == 0)
+    result = get_link_n_last_value(1,list) + list->value;
+    for (int i = 0; i < 2; ++i)
     {
-      fprintf(stderr, "Division by 0 not allowed\n");
-      exit(4);
+      list = pop_link(list);
     }
-    result = result / tmp;
-    list = pop_link(list);
-    newlink = make_link(NULL, result, list);
-  }else if (strcmp(list->operation, op[3]) == 0)
+    newlink = make_link(result,list);
+  }else if (strcmp(instruction, op[1]) == 0)
   {
-   int tmp;
-   list = pop_link(list);
-   result = get_link_n_last_value(0, list);
-   list = pop_link(list);
-   tmp = get_link_n_last_value(0, list);
-   if (tmp == 0)
-   {
-    fprintf(stderr, "Division by 0 not allowed\n");
-    exit(4);
+    result = get_link_n_last_value(1,list) - list->value;
+    for (int i = 0; i < 2; ++i)
+    {
+      list = pop_link(list);
+    }
+    newlink = make_link(result,list);
+  }else if (strcmp(instruction, op[2]) == 0)
+  {
+    result = get_link_n_last_value(1,list) * list->value;
+    for (int i = 0; i < 2; ++i)
+    {
+      list = pop_link(list);
+    }
+    newlink = make_link(result,list);
+  }else if (strcmp(instruction, op[3]) == 0)
+  {
+
+
+  }else if (strcmp(instruction, op[4]) == 0)
+  {
+
+  }else if (strcmp(instruction, op[5]) == 0)
+  {
+
+  }else if (strcmp(instruction, op[6]) == 0)
+  {
+
+  }else if (strcmp(instruction, op[7]) == 0)
+  {
+
+  }else if (strcmp(instruction, op[8]) == 0)
+  {
+
   }
-  result = result % tmp;
-  list = pop_link(list);
-  newlink = make_link(NULL, result, list);
-}else if (strcmp(list->operation, op[4]) == 0)
-{
-
-}else if (strcmp(list->operation, op[5]) == 0)
-{
-
-}else if (strcmp(list->operation, op[6]) == 0)
-{
-
-}else if (strcmp(list->operation, op[7]) == 0)
-{
-
-}else if (strcmp(list->operation, op[8]) == 0)
-{
-
-}else
-{
-
-
-}
-return newlink;
+  return newlink;
 }
 
 int main(){
@@ -211,11 +159,15 @@ int main(){
   for (int i = 0; i < N; i++) {
     char instruction[11];
     scanf("%s", instruction);
-    list = init_link(list, instruction);
+    if(is_instruction(instruction) == NULL){
+      list = init_link(list, instruction);
+    }
+    else{
+      list = do_instruction(list, instruction);
+    }
   }
 
 
-  //list = do_instruction(list);
   display_link(list);
 
 
